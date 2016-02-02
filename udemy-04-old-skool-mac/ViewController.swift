028915181878c703f590a20ba1d08d4f22994a73
David Stroud <david@dwstroud.com>
2016-02-02 10:20:41 +0000

    Initial files

--- udemy-04-old-skool-mac/ViewController.swift
+++ udemy-04-old-skool-mac/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainLbl: UILabel!
+    
+    var phrases = ["Booting from floppy...\n", "Reading from disk...\n", "Updating registry...\n", "....................\n", "....................\n", "......................................\n", "Welcome!\nNice to see you again.\n>"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        MainLbl.text = ""
+
+        // three different ways of looping...!
+
+        // method 1
+//        for var x = 0; x < phrases.count; x=x+1 {
+//            var txt = MainLbl.text!
+//            txt += phrases[x]
+//            MainLbl.text = txt
+//        }
+
+        // method 2
+        var x = 0
+        repeat {
+            var txt = MainLbl.text!
+            txt += phrases[x]
+            MainLbl.text = txt
+            x = x + 1
+        } while x < phrases.count
+        
+        // method 3
+//        for phrase in phrases {
+//            var txt = MainLbl.text!
+//            txt += phrase
+//            MainLbl.text = txt
+//        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
